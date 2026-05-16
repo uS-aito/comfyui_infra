a06e9eebe868e4d2ac742bf992e191f6fca33695
yu.saito <h2so40627@gmail.com>
2026-05-16 20:17:05 +0900

    fix code place

--- s3/cloudfront.tf
+++ s3/cloudfront.tf
@@ -1,22 +1,3 @@
-resource "aws_s3_bucket" "output" {
-  bucket = "output-${data.aws_caller_identity.current.account_id}"
-}
-
-resource "aws_s3_bucket_versioning" "output" {
-  bucket = aws_s3_bucket.output.id
-  versioning_configuration {
-    status = "Disabled"
-  }
-}
-
-resource "aws_s3_bucket_public_access_block" "output" {
-  bucket                  = aws_s3_bucket.output.id
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
-
 resource "aws_cloudfront_origin_access_control" "output" {
   name                              = "output-${data.aws_caller_identity.current.account_id}"
   origin_access_control_origin_type = "s3"
--- s3/s3.tf
+++ s3/s3.tf
@@ -18,3 +18,22 @@ resource "aws_s3_bucket_public_access_block" "models" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+
+resource "aws_s3_bucket" "output" {
+  bucket = "output-${data.aws_caller_identity.current.account_id}"
+}
+
+resource "aws_s3_bucket_versioning" "output" {
+  bucket = aws_s3_bucket.output.id
+  versioning_configuration {
+    status = "Disabled"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "output" {
+  bucket                  = aws_s3_bucket.output.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
